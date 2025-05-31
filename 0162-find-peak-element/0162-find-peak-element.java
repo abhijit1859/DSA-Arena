231db85d6f656cccca5355ddfc9266590b058296
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-05-31 21:24:55 +0530

    Time: 1 ms (2.54%), Space: 42.9 MB (13.83%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int ans = search(nums,0,nums.length-1);
+        System.out.println(ans);
+        return ans;
+    }
+
+    private static int search(int[] arr,int left,int right){
+        if(left>right){
+            return -1;
+        }
+        int mid =  (left+right)/2;
+     
+       
+        if ((mid == 0 || arr[mid] > arr[mid - 1]) && 
+            (mid == arr.length - 1 || arr[mid] > arr[mid + 1])) {
+                
+        return mid;
+        }
+
+        int leftans = search(arr,left,mid-1);
+        if(leftans!=-1) return leftans;
+      
+        
+        return search(arr,mid+1,right);
+
+    }  
+}

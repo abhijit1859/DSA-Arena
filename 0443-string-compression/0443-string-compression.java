3bdac908cb3ac19f0378c4ae43c92a0b5ae689ee
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-06-12 09:41:41 +0530

    Time: 1 ms (99.58%), Space: 44.1 MB (67.09%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -7,12 +7,13 @@ class Solution {
             if (chars[i] == chars[i - 1]) {
                 count++;
             } else {
-                chars[index++] = chars[i - 1]; // Write the char
-
+                chars[index] = chars[i - 1]; // Write the char
+                index++;
                 if (count > 1) {
                     String ctr = String.valueOf(count);
                     for (int k = 0; k < ctr.length(); k++) {
-                        chars[index++] = ctr.charAt(k);
+                        chars[index] = ctr.charAt(k);
+                        index++;
                     }
                 }
 
@@ -21,11 +22,13 @@ class Solution {
         }
 
         // Handle the last group
-        chars[index++] = chars[chars.length - 1];
+        chars[index] = chars[chars.length - 1];
+        index++;
         if (count > 1) {
             String ctr = String.valueOf(count);
             for (int k = 0; k < ctr.length(); k++) {
-                chars[index++] = ctr.charAt(k);
+                chars[index] = ctr.charAt(k);
+                index++;
             }
         }
 

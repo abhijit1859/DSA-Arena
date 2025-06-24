6651b7ee6c7b0d12408efc8db27b9bbba12d4681
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-06-24 10:32:28 +0530

    Time: 8 ms (9.62%), Space: 43.2 MB (95.73%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,24 +1,22 @@
- 
-
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);  // important for handling duplicates
-        return subset(nums);
+        Arrays.sort(nums);
+        List<List<Integer>> list = subset(nums);
+        return list;
     }
 
     private static List<List<Integer>> subset(int[] arr){
         List<List<Integer>> outer = new ArrayList<>();
-        outer.add(new ArrayList<>()); // Start with empty subset
-
-        for (int j = 0; j < arr.length; j++) {
-            int n = outer.size();
-            for (int i = 0; i < n; i++) {
+        outer.add(new ArrayList<>());
+        for(int j=0;j<arr.length;j++){
+            int n=outer.size();
+            for(int i=0;i<n;i++){
                 List<Integer> internal = new ArrayList<>(outer.get(i));
-                internal.add(arr[j]);  // Always add arr[j]
-                // Only add if it's not already in outer
-                if (!outer.contains(internal)) {
-                    outer.add(internal);
+                internal.add(arr[j]);
+                if(!outer.contains(internal)){
+                     outer.add(internal);
                 }
+               
             }
         }
 

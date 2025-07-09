6cdc24cec93409a7113597553c051156d42b7189
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-07-09 14:31:24 +0530

    Time: 7 ms (45.89%), Space: 40.1 MB (91.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -4,11 +4,8 @@ class Solution {
     }
 
     private static int fibo(int n){
-        if(n==1){
-            return 1;
-        }
-        if(n==0){
-            return 0;
+        if (n <= 1) {
+            return n;  
         }
 
         return fibo(n-1)+fibo(n-2);

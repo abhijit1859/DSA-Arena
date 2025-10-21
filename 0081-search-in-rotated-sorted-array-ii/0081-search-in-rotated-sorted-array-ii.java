11cbeddac66114402e1c417c0725518a66236dca
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-10-21 15:11:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,32 +4,33 @@ class Solution {
     }
 
     private static boolean find(int[] arr,int target){
-        int low=0;
-        int high=arr.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if (arr[left] == arr[mid] && arr[mid] == arr[right]) {
+                left++;
+                right--;
+                continue;
+            }
+
             if(arr[mid]==target){
                 return true;
             }
-            if(arr[low]==arr[mid]&&arr[high]==arr[mid]){
-                low++;
-                high--;
-                continue;
-            }
-            if(arr[low]<=arr[mid]){
-                if(target>=arr[low]&&target<arr[mid]){
-                    high=mid-1;
+
+            if(arr[mid]>=arr[left]){
+                if(target>=arr[left]&&target<arr[mid]){
+                    right=mid-1;
                 }else{
-                    low=mid+1;
+                    left=mid+1;
                 }
             }else{
-                if(target>arr[mid]&&target<=arr[high]){
-                    low=mid+1;
+                if(target>arr[mid]&&target<=arr[right]){
+                    left=mid+1;
                 }else{
-                    high=mid-1;
+                    right=mid-1;
                 }
             }
-
         }
         return false;
     }

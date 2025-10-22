46064d6b0e355878bb5ad200ae8dec2a0e5e2d15
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-10-22 08:28:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,28 +1,28 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int ans = search(nums,0,nums.length-1);
-        System.out.println(ans);
-        return ans;
+        return findPeak(nums);
     }
 
-    private static int search(int[] arr,int left,int right){
-        if(left>right){
-            return -1;
-        }
-        int mid =  (left+right)/2;
-     
-       
-        if ((mid == 0 || arr[mid] > arr[mid - 1]) && 
-            (mid == arr.length - 1 || arr[mid] > arr[mid + 1])) {
-                
-        return mid;
-        }
-
-        int leftans = search(arr,left,mid-1);
-        if(leftans!=-1) return leftans;
-      
+    private static int findPeak(int[] arr){
         
-        return search(arr,mid+1,right);
+        int n=arr.length;
 
-    }  
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 1;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        int left=1;
+        int right=n-2;
+
+        
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+           if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]) return mid;
+           else if(arr[mid]<arr[mid-1]) return right=mid-1;
+           else if(arr[mid]<arr[mid+1]) return left=mid+1;
+        }
+
+        return -1;
+    }
 }

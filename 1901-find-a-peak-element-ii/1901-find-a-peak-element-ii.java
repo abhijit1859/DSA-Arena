1d25352493f501cb6b7c66c88b36be52870d1ce4
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-10-29 17:10:06 +0530

    Time: 0 ms (100%), Space: 84.2 MB (27.48%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -6,15 +6,24 @@ class Solution {
     private static int[] findPeak(int[][] mat){
         int m=mat.length;
         int n=mat[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int up=(i>0)?mat[i-1][j]:-1;
-                int down=(i<m-1)?mat[i+1][j]:-1;
-                int left=(j>0)?mat[i][j-1]:-1;
-                int right=(j<n-1)?mat[i][j+1]:-1;
-                if(mat[i][j]>up&&mat[i][j]>down&&mat[i][j]>left&&mat[i][j]>right){
-                    return new int[]{i,j};
+        int left=0;
+        int right=mat[0].length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            int max=0;
+            for(int i=0;i<m;i++){
+                if(mat[i][mid]>mat[max][mid]){
+                    max=i;
                 }
+            }
+            int leftN = (mid-1>0)?mat[max][mid-1]:-1;
+            int rightN = (mid+1<n)?mat[max][mid+1]:-1;
+            if(mat[max][mid]>leftN&&mat[max][mid]>rightN){
+                return new int[]{max,mid};
+            }else if(mat[max][mid]<leftN){
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
 

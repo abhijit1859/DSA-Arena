6421e51a496d54fafe2e401070aa0091356e5891
Abhijit Kumar <abhijitkumar1859@gmail.com>
2025-12-30 21:29:49 +0530

    Time: 7 ms (21.44%), Space: 17 MB (10.28%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> map;
+        for(char c:s){
+            map[c]++;
+        }
+        int n=s.size();
+        vector<vector<char>> bucket(n+1);
+
+        for(auto &it:map){
+            bucket[it.second].push_back(it.first);
+        }
+
+        string result="";
+
+        for(int i=n;i>0;i--){
+            for(char c:bucket[i]){
+                result.append(i,c);
+            }
+        }
+
+        return result;
+
+
+
+        
+        
+    }
+};

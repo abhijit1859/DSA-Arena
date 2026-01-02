1b6c0a4ed34f9ef7b3984f11b43eebe3021ea41f
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-01-02 20:49:51 +0530

    Time: 2 ms (46.92%), Space: 12.9 MB (13.2%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,21 +4,18 @@ public:
         unordered_map<int,int> mp;
         stack<int> s;
         for(int i=nums2.size()-1;i>=0;i--){
-            while(!s.empty()&&s.top()<=nums2[i]){
+            int el=nums2[i];
+            while(!s.empty()&&s.top()<=el){
                 s.pop();
             }
-            mp[nums2[i]]=s.empty()?-1:s.top();
-            s.push(nums2[i]);
-
-
+            mp[el]=s.empty()?-1:s.top();
+            s.push(el);
         }
-        
-            vector<int> res;
-            for(int num:nums1){
-                res.push_back(mp[num]);
-            }
+        vector<int> ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mp[nums1[i]]);
+        }
 
-            return res;
-        
+        return ans;
     }
 };

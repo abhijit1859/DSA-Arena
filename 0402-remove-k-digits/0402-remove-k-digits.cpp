4589cc6fc782e8b9d194d537c2aa49c6a4634c7a
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-01-07 21:04:28 +0530

    Time: 3 ms (61.44%), Space: 11.8 MB (26.6%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -3,16 +3,20 @@ public:
     string removeKdigits(string num, int k) {
         stack<char> st;
         int n=num.size();
-        for(char c : num){
-            while(!st.empty()&&k>0&&st.top()>c){
+        for(char c:num){
+            
+            while(!st.empty()&&st.top()>c&&k>0){
                 st.pop();
                 k--;
             }
+
             st.push(c);
         }
+
         while(k>0&&!st.empty()){
             st.pop();
             k--;
+
         }
 
         string ans;
@@ -20,15 +24,13 @@ public:
             ans.push_back(st.top());
             st.pop();
         }
-
-        
         reverse(ans.begin(),ans.end());
         int idx=0;
-        while(idx<ans.size()&&ans[idx]=='0'){
+        while(idx<n&&ans[idx]=='0'){
             idx++;
-           
         }
         ans=ans.substr(idx);
         return ans.empty()?"0":ans;
+        
     }
 };

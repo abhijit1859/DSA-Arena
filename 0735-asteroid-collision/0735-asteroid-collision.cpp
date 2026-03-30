93a7b9a9f82a40f29e63be4902878b4fd72861b9
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-03-30 18:52:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-        for (int a : asteroids) {
+        for(int a:asteroids){
             if(a>0||st.empty()){
                 st.push(a);
             }else{
@@ -12,24 +12,23 @@ public:
                 if(!st.empty()&&st.top()==abs(a)){
                     st.pop();
                 }else{
+
                     if(st.empty()||st.top()<0){
                         st.push(a);
                     }
                 }
+              
             }
         }
 
-      
-    
-
         vector<int> result;
         while(!st.empty()){
-            int num=st.top();
-            result.push_back(num);
+            result.push_back(st.top());
             st.pop();
         }
-        reverse(result.begin(),result.end());
 
+        reverse(result.begin(),result.end());
         return result;
+
     }
 };

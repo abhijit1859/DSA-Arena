bbc36c753bf43ca26007b1a557fe226a130082bd
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-07-02 23:58:09 +0530

    Time: 19 ms (81.4%), Space: 70.6 MB (47.19%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,47 +1,50 @@
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        if((long long)m*k>bloomDay.size()){
-            return -1;
-        }
-        int low=1;
-        int high=0;
-        for(int day:bloomDay){
-            high=max(high,day);
+    bool canBloom(vector<int>& bloomDays, int m, int k, int day) {
+        int consecutive = 0;
+        int bouquets = 0;
+
+        for (int i = 0; i < bloomDays.size(); i++) {
+            if (bloomDays[i] <= day) {
+                consecutive++;
+            } else {
+                consecutive = 0;
+            }
+
+            if (consecutive == k) {
+                bouquets++;
+                consecutive = 0;
+
+                if (bouquets == m)
+                    return true;
+            }
         }
 
-        while(low<=high){
-            int mid=(high-low)/2+low;
-            if(bloom(bloomDay,m,k,mid)){
-                high=mid-1;
-            }else{
-                low=mid+1;
+        return false;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        if ((long long)m * k > bloomDay.size())
+            return -1;
+
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        for (int d : bloomDay) {
+            low = min(low, d);
+            high = max(high, d);
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canBloom(bloomDay, m, k, mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-          
         }
 
         return low;
-
-    }
-
-    bool bloom(vector<int>& bloomDay,int m,int k,int day){
-        int c=0;
-        int bq=0;
-        for(int i=0;i<bloomDay.size();i++){
-            if(bloomDay[i]<=day){
-                c++;
-            }else{
-                c=0;
-            }
-            if(c==k){
-                bq++;
-                c=0;
-                if(bq==m){
-                    return true;
-                }
-            }
-
-        }
-        return false;
     }
 };

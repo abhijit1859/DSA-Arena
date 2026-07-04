354fc2cbf606d74378ed90bffbb979113499d206
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-07-04 10:38:49 +0530

    Time: 0 ms (100%), Space: 10.5 MB (72.43%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -7,34 +7,29 @@ public:
             high+=num;
         }
 
-        int ans=-1;
-
         while(low<=high){
-            int mid=(high-low)/2+low;
-            if(isPossible(nums,mid,k)){
-                ans=mid;
+            int mid=low+(high-low)/2;
+            if(canSplit(nums,mid,k)){
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
 
-    bool isPossible(vector<int>& arr,int max,int k){
-        int part=1;
+    bool canSplit(vector<int>& nums,int max,int k){
+        int count=1;
         int sum=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]+sum>max){
-                sum=arr[i];
-                part++;
-                if(part>k){
-                    return false;
-                }
+        for(int num:nums){
+            if(sum+num>max){
+                count++;
+                sum=num;
             }else{
-                sum+=arr[i];
+                sum+=num;
             }
         }
-        return true;
+
+        return count<=k;
     }
 };

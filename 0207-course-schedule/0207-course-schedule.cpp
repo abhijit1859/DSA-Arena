a48248f6f55461de15d04ee6d49b3b2d87123a64
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-07-31 08:17:59 +0530

    Time: 7 ms (44.92%), Space: 19.1 MB (81.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        vector<int> indegree(numCourses,0);
-        for(auto &it:prerequisites){
-            int u=it[0];
-            int v=it[1];
-            adj[v].push_back(u);
-            indegree[u]++;
-        }
-
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-
-        int count=0;
-        while(!q.empty()){
-            int u=q.front();
-            q.pop();
-            count++;
-            for(int v:adj[u]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.push(v);
+    bool dfs(int node,vector<int>& state,vector<vector<int>>& adj){
+        state[node]=1;
+        for(auto& neigh:adj[node]){
+            if(state[neigh]==0){
+                if(dfs(neigh,state,adj)){
+                    return true;
                 }
             }
+            if(state[neigh]==1){
+                return true;
+            }
         }
-
-        if(numCourses==count){
-            return true;
-        }
-
+        state[node]=2;
         return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto &p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
 
+        vector<int> state(numCourses,0);
+
+        for(int i=0;i<numCourses;i++){
+            if(dfs(i,state,adj)){
+                return false;
+            }
+        }
+        return true;
     }
 };

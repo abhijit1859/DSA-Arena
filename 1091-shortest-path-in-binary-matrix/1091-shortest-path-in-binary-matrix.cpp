b4d600c7d93195fabe5e7e471e593d985a60eef6
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-07-31 11:42:43 +0530

    Time: 9 ms (92.43%), Space: 23.3 MB (97.34%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,49 +2,35 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
+        if(grid[0][0]!=0||grid[n-1][n-1]!=0) return -1;
 
-        if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
-        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
-
-         priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
-        vector<pair<int,int>> dir = {
-            {-1,-1}, {-1,0}, {-1,1},
-            {0,-1},          {0,1},
-            {1,-1},  {1,0},  {1,1}
-        };
-        dist[0][0]=1;
-        pq.push({1,{0,0}});
-
-        while(!pq.empty()){
-            auto [d,cell]=pq.top();
-            pq.pop();
-            int x=cell.first;
-            int y=cell.second;
-
-
-           
-            for(auto &[dx,dy]:dir){
-                int nx=x+dx;
-                int ny=y+dy;
-
-                if(nx>=0&&ny>=0&&nx<n&&ny<n&&grid[nx][ny]==0){
-                    if(d+1<dist[nx][ny]){
-                        dist[nx][ny]=d+1;
-                        pq.push({d+1,{nx,ny}});
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dc[] = {-1,  0,  1,-1, 1,-1, 0, 1};
+        grid[0][0]=1; //visited
+        int dist=1;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                auto [r,c]=q.front();
+                q.pop();
+                if(r==n-1&&c==n-1) return dist;
+                for(int i=0;i<8;i++){
+                    int nr=r+dr[i];
+                    int nc=c+dc[i];
+                    if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]==0){
+                        grid[nr][nc]=1;//mark visited and push it into queue;
+                        q.push({nr,nc});
                     }
                 }
+
             }
+            dist++;
+
         }
 
-        if(dist[n-1][n-1]==INT_MAX) return -1;
-
-        return dist[n-1][n-1];
-
-
-
+        return -1;
+        
     }
 };

62d6d12752097b244c6b883c59acfb3d53516f43
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-08-03 17:37:08 +0530

    Time: 86 ms (45.81%), Space: 44.5 MB (53.38%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n+1];
-        for(auto &it:times){
-            int u=it[0];
-            int v=it[1];
-            int w=it[2];
-            adj[u].push_back({v,w});
+        vector<vector<pair<int,int>>> adj(n+1);
+        for (auto& edges : times) {
+            int u = edges[0];
+            int v = edges[1];
+            int w = edges[2];
+            adj[u].push_back({v, w});
         }
-        //distance node
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-        vector<int> dist(n+1,INT_MAX);
-        dist[k]=0;
-        pq.push({0,k});
-        while(!pq.empty()){
-            auto [d,node]=pq.top();
-            pq.pop();
-            for(auto &it:adj[node]){
-                int neigh=it.first;
-                int wt=it.second;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
 
-                if(d+wt<dist[neigh]){
-                    dist[neigh]=d+wt;
-                    pq.push({dist[neigh],neigh});
+        // dist,node
+        vector<int> dist(n + 1, INT_MAX);
+        pq.push({0, k});
+        dist[k] = 0;
+
+        while (!pq.empty()) {
+            auto [currDist, node] = pq.top();
+            pq.pop();
+            if(currDist>dist[node]) continue;
+
+            for (auto& it : adj[node]) {
+                int neigh = it.first;
+                int weight = it.second;
+                if (currDist + weight < dist[neigh]) {
+                    dist[neigh] = currDist + weight;
+                    pq.push({currDist + weight, neigh});
                 }
             }
         }
-
-        int ans=0;
-
-        for(int i=1;i<=n;i++){
-            if(dist[i]==INT_MAX) return -1;
-            ans=max(ans,dist[i]);
+        int ans = INT_MIN;
+        for (int i = 1; i < dist.size(); i++) {
+            if (dist[i] == INT_MAX) {
+                return -1;
+            } else {
+                ans = max(ans, dist[i]);
+            }
         }
         return ans;
-
     }
 };

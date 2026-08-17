2ee0157550a1d30f411d04402f130f1fac615f19
Abhijit Kumar <abhijitkumar1859@gmail.com>
2026-08-17 07:40:40 +0530

    Time: 0 ms (100%), Space: 9.7 MB (12.04%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> result;
+    int sum=0;
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> temp;
-        genSub(temp,1,0,k,n);
+        gen(k,n,temp,1);
         return result;
     }
 
-    void genSub(vector<int> &temp,int curr,int sum,int k,int n){
-
+    void gen(int k,int n,vector<int> temp,int curr){
         if(temp.size()==k){
             if(sum==n){
                 result.push_back(temp);
@@ -18,15 +18,12 @@ public:
 
         if(sum>n) return;
 
-
         for(int i=curr;i<=9;i++){
-           
             temp.push_back(i);
-            genSub(temp,i+1,sum+i,k,n);
+            sum+=i;
+            gen(k,n,temp,i+1);
+            sum-=i;
             temp.pop_back();
-          
         }
     }
-
-
 };
